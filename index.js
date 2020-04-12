aea166edadf543a604487f6ac00e45632f3789a7
visualldata <visualldata@gmail.com>
2020-04-12 19:23:53 -0300

    Updates

--- index.js
+++
@@ -1,8 +0,0 @@
-console.info("© 2020 VisuallData. All Rights Reserved");
-
-const setWidth = () => {
-  document.getElementsByTagName("img").item(0).width = window.innerWidth * 0.8;
-};
-
-window.addEventListener("resize", setWidth);
-window.addEventListener("load", setWidth);
---
+++ precache-manifest.d51d2ce455be467b9852ce991c223c7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83760aa460dff5949690d9d59fb1341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ba94288c59b6bc86e6",
+    "url": "/static/css/main.07ae030a.chunk.css"
+  },
+  {
+    "revision": "f905374ba4d0c6aea862",
+    "url": "/static/js/2.434d7ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.434d7ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ba94288c59b6bc86e6",
+    "url": "/static/js/main.8b94343e.chunk.js"
+  },
+  {
+    "revision": "69dfc88142468e520bf3",
+    "url": "/static/js/runtime-main.78311062.js"
+  },
+  {
+    "revision": "9bf516a7d003bab6eb91247ed4967e07",
+    "url": "/static/media/ipatinga.9bf516a7.svg"
+  }
+]);
